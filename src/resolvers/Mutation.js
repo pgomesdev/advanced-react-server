742437461dbc025867bfb8ec5c50cac5fec3bc2e
pgomesdev <gomes.pedro15@gmail.com>
2019-02-18 21:31:56 -0300

    client side graphql class changes

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -10,6 +10,29 @@ const Mutation = {
 
     return item;
   },
+  updateItem: (parent, args, context, info) => {
+    // first take a copy of the updates
+    const updates = { ...args }
+
+    // remove the id from the updates
+    delete updates.id
+
+    // run the update method
+    return context.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id,
+      },
+    }, info)
+  },
+  deleteItem: async (parent, args, context, info) => {
+    const where = { id: args.id }
+
+    const item = await context.db.query.item({ where }, `{ id title }`)
+    // 2. TODO: check if they own that item, or have the permissions]
+    // 3. delete it
+    return context.db.mutation.deleteItem({ where }, info)
+  }
 };
 
 module.exports = Mutation;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,11 @@
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-  items: (parent, args, context, info) => {
-    return context.db.query.items();
-  }
+  items: forwardTo('db'),
+  item: (parent, { where }, context, info) => {
+    return context.db.query.item({ where });
+  },
+  itemsConnection: forwardTo('db'),
 };
 
 module.exports = Query;
