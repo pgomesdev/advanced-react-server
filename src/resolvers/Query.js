e2b471a7b1dbf32348d580ee5fd65c4d55a11132
pgomesdev <gomes.pedro15@gmail.com>
2019-02-20 21:01:23 -0300

    currently logged in user class

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -6,6 +6,17 @@ const Query = {
     return context.db.query.item({ where });
   },
   itemsConnection: forwardTo('db'),
+  me: (parent, args, context, info) => {
+    if (!context.request.userId) {
+      return null
+    }
+
+    return context.db.query.user({
+      where: {
+        id: context.request.userId,
+      },
+    }, info)
+  },
 };
 
 module.exports = Query;
