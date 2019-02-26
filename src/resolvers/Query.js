c33cdff74649b8f7bba31672586ce531766b35a5
pgomesdev <gomes.pedro15@gmail.com>
2019-02-26 19:48:18 -0300

    permissions management class

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils')
 
 const Query = {
   items: forwardTo('db'),
@@ -16,6 +17,15 @@ const Query = {
         id: context.request.userId,
       },
     }, info)
+  },
+  users: async (parent, args, context, info) => {
+    if (!context.request.userId) {
+      throw new Error('You must be logged in!')
+    }
+
+    hasPermission(context.request.user, ['ADMIN', 'PERMISSIONUPDATE'])
+
+    return context.db.query.users({}, info)
   },
 };
 
